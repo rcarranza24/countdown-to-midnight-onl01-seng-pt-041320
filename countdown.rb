991a1cea9efad2a260a5841ad8df66a466aaf747
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-15 14:05:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 def countdown(num)
-  while num > 0
+while num > 0
   puts "#{num} SECOND(S)!"
   num -= 1
 end
-puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
+
 
 def countdown_with_sleep(num)
   while num > 0
